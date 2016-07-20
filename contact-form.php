9a68a1fed50257cb63b4dd9175091af2f1166b99
sitypop <sityemilyshah@gmail.com>
2016-07-20 12:11:52 +0100

    Adds trailing slash to redirect & adds in post method

--- contact-form.php
+++ contact-form.php
@@ -1,9 +1,9 @@
 <?php
 
 $recipient = "sityemilyshah@gmail.com";
-$successPage = "index.html";
+$successPage = "index.html/";
 
-
+//import form information
 $name = $_POST['name'];
 $email = $_POST['email'];
 $subject = $_POST['subject'];
