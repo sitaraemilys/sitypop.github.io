01391ed7237720ef9aed1fd817b0399a4264b7a4
sitypop <sityemilyshah@gmail.com>
2019-02-05 16:56:32 +0000

    MVP new design

--- js/interface.js
+++ js/interface.js
@@ -1,45 +1,6 @@
-jQuery(function($) {
-  "use strict";
+$(document).ready(function() {
 
-$('.navigation').singlePageNav({
-        currentClass: 'active',
-        changeHash: true,
-        scrollSpeed: 750,
-        offset: 40,
-        filter: ':not(.external)',
-        easing: 'swing'
+    $('.shape-one').draggable();
+    $('.shape-two').draggable();
 
-    });
-
-    $.noConflict();
-     $('.nav a').on('click', function(){
-        if($('.navbar-toggle').css('display') !='none'){
-            $(".navbar-toggle").trigger( "click" );
-        }
-    });
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
 });
-
-
-
-// wow animation
-
- new WOW().init();
-
-
- });
-
- // ghost typer
-
-//  $("#title").ghosttyper({
-//   messages: ['Frontend Developer', 'Web Designer', 'Artist', 'Surfer Yogi Chick' ],
-//   // animation speed for typing effect
-//   timeWrite: 100,
-//   // animation speed for deleting effect
-//   timeDelete: 50,
-//   // animation delay
-//   timePause: 1000,
-//   callback: function(){}
-// });
