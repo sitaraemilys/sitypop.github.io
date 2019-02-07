f7867d133be92913b9d1ef348c60ad784aa3b283
sitypop <sityemilyshah@gmail.com>
2019-02-07 10:51:10 +0000

    Adds cheeky touch

--- js/interface.js
+++ js/interface.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
 
+
     $('.shape-one').draggable();
     $('.gif').draggable();
+
+    var naked = false;
+
+    $( ".gif" ).mousedown(function() {
+      if (naked == false) {
+          $('.gif').attr('src','./images/gif/dancin-naked.gif');
+          naked = true;
+      } else {
+          $('.gif').attr('src','./images/gif/dancin.gif');
+          naked = false;
+      }
+    });
+
 
 });
